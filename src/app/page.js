d0e85c5f55d88d046c65c554d58ebc2fbfff1280
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-10 18:08:22 +0000

    Changes to be committed: 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HeroSection from "./components/hero/Hero";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center py-16 bg-slate-50 dark:bg-slate-50 px-5  w-full">
+    <main className="flex flex-col items-center justify-center py-16 bg-slate-50 dark:bg-slate-50 px-3  w-full">
       <HeroSection/>
     </main>
   );
