205ef511a209b40ab3db4397b5f327e386ddf0ec
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-10 17:45:15 +0000

    Changes to be committed: 	modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
   description:
     "Luxury Limo Here",
   openGraph: {
-    // icon: "/logo.png",
+    icon: "https://images.unsplash.com/photo-1485291571150-772bcfc10da5?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     images:
       "https://images.unsplash.com/photo-1485291571150-772bcfc10da5?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     // url: "https://colossal-mindand-cake.vercel.app/",
