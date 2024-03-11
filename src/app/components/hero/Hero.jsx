6f4cfb9c33cac3fb1f6594c8f430cc41a99553c8
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-11 08:29:41 +0000

    Changes to be committed: 	modified:   src/app/components/hero/Hero.jsx

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div className="flex items-center px-14 sm:px-2 w-full max-w-7xl  h-[90dvh] rounded-3xl bg-center bg-cover sm:bg-[url('https://images.unsplash.com/photo-1601929862217-f1bf94503333?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]  bg-[url('https://images.unsplash.com/photo-1485291571150-772bcfc10da5?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
-     <h1 className='text-slate-50 sm:text-slate-950 text-4xl text-center font-bold'>Luxury limo hire</h1>
-     <p className='text-center text-slate-50 sm:text-slate-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate?</p>
+    <div className="flex flex-col items-center px-14 sm:px-2 w-full max-w-7xl  h-[90dvh] rounded-3xl bg-center bg-cover sm:bg-[url('https://images.unsplash.com/photo-1601929862217-f1bf94503333?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]  bg-[url('https://images.unsplash.com/photo-1485291571150-772bcfc10da5?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+     <h1 className='text-slate-50  text-4xl text-center font-bold'>Luxury limo hire</h1>
+     <p className='text-center text-slate-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate?</p>
     </div>
   )
 }
